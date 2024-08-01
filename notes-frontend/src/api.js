9b6bf77266ce40b5e9ac772689f99dca4589c9c1
Am10aN16 <2020071013@mmmut.ac.in>
2024-08-02 04:02:55 +0530

    project completed

--- notes-frontend/src/api.js
+++ notes-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://notes-app-lsdn.onrender.com/api'
 });
 
 export const createGroup = (data) => api.post('/groups/create', data);
