991f1468044b43ecae171f5b3df0fdeff5fa355b
Am10aN16 <2020071013@mmmut.ac.in>
2024-08-02 03:51:17 +0530

    project completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require("path");
 require('dotenv').config();  
 
 const app = express();
@@ -23,6 +24,18 @@ const noteRoutes = require('./routes/note');
 app.use('/api/groups', groupRoutes);
 app.use('/api/notes', noteRoutes);
 
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, '/notes-frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname1, 'notes-frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("Hello from the server")
+    })
+}
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
